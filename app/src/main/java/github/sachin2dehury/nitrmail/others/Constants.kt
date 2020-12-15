9ce3b3fb6b1e5ad6985109b9f4a915766a9e0f3d
Sachin Dehury <51115041+Sachin2Dehury@users.noreply.github.com>
2020-12-15 14:29:00 +0530

    Kotlin version updated.

--- app/src/main/java/github/sachin2dehury/nitrmail/others/Constants.kt
+++ app/src/main/java/github/sachin2dehury/nitrmail/others/Constants.kt
@@ -2,11 +2,14 @@ package github.sachin2dehury.nitrmail.others
 
 object Constants {
 
-    const val INBOX_URL = "https://mail.nitrkl.ac.in/home/~/inbox?&fmt=json"
-    const val SENT_URL = "https://mail.nitrkl.ac.in/home/~/sent?&fmt=json"
-    const val DRAFT_URL = "https://mail.nitrkl.ac.in/home/~/drafts?&fmt=json"
-    const val CONTACTS_URL = "https://mail.nitrkl.ac.in/home/~/contacts?&fmt=json"
-    const val TASKS_URL = "https://mail.nitrkl.ac.in/home/~/tasks?&fmt=json"
+    const val INBOX_URL = "https://mail.nitrkl.ac.in/home/~/inbox.json"
+    const val SENT_URL = "https://mail.nitrkl.ac.in/home/~/sent.json"
+    const val DRAFT_URL = "https://mail.nitrkl.ac.in/home/~/drafts.json"
+    const val CONTACTS_URL = "https://mail.nitrkl.ac.in/home/~/contacts.json"
+    const val TASKS_URL = "https://mail.nitrkl.ac.in/home/~/tasks.json"
+    const val JUNK_URL = "https://mail.nitrkl.ac.in/home/~/junk.json"
+    const val TRASH_URL = "https://mail.nitrkl.ac.in/home/~/trash.json"
+    const val CALENDAR_URL = "https://mail.nitrkl.ac.in/home/~/calendar.json"
     const val ITEM_URL = "https://mail.nitrkl.ac.in/home/~/?id="
 
     const val MIME_TAG = "MIME-Version: 1.0"
